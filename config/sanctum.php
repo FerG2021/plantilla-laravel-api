80917bc36e3f9f306da66db15ac37c52ca7632a9
Fernando Gonzalez <fernandojaviergonzalez2018@gmail.com>
2022-09-04 01:15:23 -0300

    agregada la url para prueba

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,balanceado.com.ar,66.97.40.211',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
